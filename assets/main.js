5ca36013e03cf808097b8ea88cb6d3e738007045
marielosagui <00078913@uca.edu.sv>
2018-10-12 09:21:16 -0600

    modificaciones

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,21 @@
-var container = document.getElementById("c1")
-container.innerText="Hola Mundo";
+var container = document.getElementById("c1");
+container.innerText="¡Hola Mundo!";
 container.style.backgroundColor= "#000000";
 container.style.color="#1cb723";
 container.style.width="200px";
 container.style.height="200px";
-container.innerHTML = "<input type='text' placeholder='Escriba su text'>";
+container.innerHTML= "<input type='text' placeholder='escriba su texto'></input>";
 
-var containers= document.getElementsByClassName("c2");
-container.forEach(function(element){
-    element.style.backgroundColor="009999";
+var containers=document.querySelectorAll(".c2");
+containers.forEach(function(element) {
+ element.style.backgroundColor="#009999";    
 });
 
-container.forEach(function(element){
-    element.innerHTML ="<input type='text' placeholder ='Escriba su texto'>";
-});
+containers.forEach(function(element) {
+    element.innerHTML= "<input type='text' placeholder='escriba su texto'></input>";
+   });
 
-var boton= document.getElementById("btn-click");
-boton.onclick=function(evt){
-    alert("Hola Mundo");
-};
-
-var btnCopy=document.getElementById("btnCopy");
-btnCopy.onclick =function(evt){
-    let msj = document.getELementById('textMsj').value;
-    document.getElementById('showMsj').innerText=msj;
-}
+   var boton= document.getElementById("btn-click");
+   boton.onclick=function(evt){
+    
+   };
